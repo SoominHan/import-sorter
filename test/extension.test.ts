b2bbf481971b98e0c314e81b007570c7f1140f25
SooMin Han <soomin.han.it@gmail.com>
2017-03-07 01:24:45 +1300

    adding testing file

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -2,21 +2,26 @@
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
-
 // The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
-
+//import * as assert from 'assert';
+import 'mocha';
+import { AstWalker, ImportCreator, ImportSorter, defaultSortConfiguration, defaultImportStringConfiguration } from "../src/core";
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-import * as myExtension from '../src/extension';
+//import * as vscode from 'vscode';
+//import * as importSorter from '../src/extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+suite('Extension Tests', () => {
+    test('just a main method', () => {
+        const walker = new AstWalker();
+        const sorter = new ImportSorter(defaultSortConfiguration);
+        const importCreator = new ImportCreator(defaultImportStringConfiguration);
+        const imports = walker.parseImports('../../../test/test-file1.txt');
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+        const sortedImports = sorter.sortImportElements(imports);
+        const importStrings = importCreator.createImportStrings(sortedImports);
+        console.log(JSON.stringify(importStrings, null, 2));
+
     });
 });
