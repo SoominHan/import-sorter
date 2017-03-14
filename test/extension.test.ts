f1bdc7d628eddcfe2780a995a728c33254924e3b
SOOMINA\Soomina <shan238@aucklanduni.ac.nz>
2017-03-15 00:26:46 +1300

    added ast-walker tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,30 +1,119 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-// The module 'assert' provides assertion methods from node
-//import * as assert from 'assert';
 import 'mocha';
-import { AstWalker, ImportCreator, ImportSorter, defaultSortConfiguration, defaultImportStringConfiguration } from '../src/core';
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-//import * as vscode from 'vscode';
-//import * as importSorter from '../src/extension';
+import * as expect from 'expect.js';
+import { ImportElement, AstWalker } from '../src/core';
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite('Extension Tests', () => {
-    test('just a main method', () => {
+interface AstTest {
+    name: string;
+    text: string;
+    expected: ImportElement;
+}
+const testArray: AstTest[] = [
+    {
+        name: 'test0',
+        text: ``,
+        expected: {
+            endPosition: { line: 0, character: 0 },
+            moduleSpecifierName: null,
+            hasFromKeyWord: false,
+            startPosition: { line: 0, character: 0 }
+        }
+
+    },
+    {
+        name: 'test1a',
+        text: `import { a, c as cc, b } from 'test.js';`,
+        expected: {
+            endPosition: { line: 0, character: 40 },
+            moduleSpecifierName: 'test.js',
+            hasFromKeyWord: true,
+            namedBindings: [
+                { name: 'a', aliasName: null },
+                { name: 'c', aliasName: 'cc' },
+                { name: 'b', aliasName: null }
+            ],
+            startPosition: { line: 0, character: 0 }
+        }
+
+    },
+
+    {
+        name: 'test1b',
+        text: `//comment
+            import  {  a  , 
+                    c  as  cc , b 
+                } 
+                from 'test.js';    
+                `,
+        expected: {
+            moduleSpecifierName: 'test.js',
+            startPosition: { line: 1, character: 3 },
+            endPosition: { line: 6, character: 20 },
+            hasFromKeyWord: true,
+            defaultImportName: undefined,
+            namedBindings: [
+                { name: 'a', aliasName: null },
+                { name: 'c', aliasName: 'cc' },
+                { name: 'b', aliasName: null }
+            ]
+        }
+    },
+    {
+        name: 'test1c',
+        text: `import { a, c as cc, b } from "test.js";`,
+        expected: {
+            endPosition: { line: 0, character: 40 },
+            moduleSpecifierName: 'test.js',
+            hasFromKeyWord: true,
+            namedBindings: [
+                { name: 'a', aliasName: null },
+                { name: 'c', aliasName: 'cc' },
+                { name: 'b', aliasName: null }
+            ],
+            startPosition: { line: 0, character: 0 }
+        }
+    }
+];
+
+suite('AstWalker Tests', () => {
+
+    const getImports = (text: string) => {
         const walker = new AstWalker();
-        const sorter = new ImportSorter();
-        sorter.initialise(defaultSortConfiguration);
-        const importCreator = new ImportCreator();
-        importCreator.initialise(defaultImportStringConfiguration);
-        const imports = walker.parseImports('../../../test/test-file1.txt');
-
-        const sortedImports = sorter.sortImportElements(imports);
-        const text = importCreator.createImportText(sortedImports.groups);
-        console.log(text);
-        //console.log(JSON.stringify(importStrings, null, 2));
-
+        const imports = walker.parseImports('nonExistantFile', text);
+        return imports;
+    };
+    const testing = (testName, text, expected) => {
+        test(`AstWalker:  ${testName} produces correct result`, () => {
+            const imports = getImports(text);
+            expect(imports.length).to.be(1);
+            expect(imports[0]).to.eql(expected);
+        });
+    };
+    testArray.forEach(testElement => {
+        testing(testElement.name, testElement.text, testElement.expected);
     });
+
 });
+
+// suite('Import Creater Tests', () => {
+// //configuration testcase as default 
+// //import elements of all kind 
+//     const creater = new ImportCreator();
+//     const importString = creater.createImportStrings(elements: ImportElement);
+//     const importText = creater.createImportText(importString);
+
+//     const getImports = (text: string) => {
+//         const walker = new AstWalker();
+//         const imports = walker.parseImports('nonExistantFile', text);
+//         return imports;
+//     };
+//     const testing = (testName, text, expected) => {
+//         test(`AstWalker:  ${testName} produces correct result`, () => {
+//             const imports = getImports(text);
+//             expect(imports.length).to.be(1);
+//             expect(imports[0]).to.eql(expected);
+//         });
+//     };
+//     testArray.forEach(testElement => {
+//         testing(testElement.name, testElement.text, testElement.expected);
+//     });
+// });
