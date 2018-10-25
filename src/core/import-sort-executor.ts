38ee6a7cb728c8c49676bb240fef14edbbb36390
mike-co <michael.litv@gmail.com>
2018-10-26 00:01:13 +1300

    adding async fork join

--- src/core/import-sort-executor.ts
+++ src/core/import-sort-executor.ts
@@ -1,10 +1,12 @@
-import { flatMap } from 'lodash';
-import { Range } from 'vscode';
+//import { flatMap } from 'lodash';
+//import { Range } from 'vscode';
 
 import * as io from '../helpers/io';
 import { IAstWalker } from './ast-walker';
 import { IImportCreator } from './import-creator';
 import { IImportSorter } from './import-sorter';
+import { Observable, merge as mergeObservable, forkJoin as forkJoinObservable } from 'rxjs';
+import { switchMap as switchMapObservable, map as mapObservable, mergeAll, filter as filterObservable } from 'rxjs/operators';
 
 export class ImportSortExecuter {
     constructor(private walker: IAstWalker, private sorter: IImportSorter, private importCreator: IImportCreator) { }
@@ -14,36 +16,48 @@ export class ImportSortExecuter {
     // }
 
     public sortImportsInDirectory(filePath: string): Promise<void> {
-        const allSourceFiles = this.getAllFilePathsUnderThePath(filePath).then(filePaths => {
-            return Promise.all(filePaths.map(path => io.readFile(path).then(fileSource => ({ path, data: fileSource }))));
-        });
-
-        return allSourceFiles.then(fileSources => {
-            const sortedSources = fileSources.map(source => this.getSortedSource(source.path, source.data));
-            return Promise.all(sortedSources).then(_ => void 0);
-        });
+        const allUpdatedSourceFiles$ = this.allFilesUnderThePath$(filePath).pipe(mapObservable(data => {
+            const sortedSourceFile = this.getSortedSource(data.filePath, data.file);
+            return { filePath: data.filePath, sortedSourceFile: sortedSourceFile };
+        }));
+        const allWrites$ = allUpdatedSourceFiles$.pipe(
+            filterObservable(data => data && data.sortedSourceFile !== null),
+            switchMapObservable(data => io.writeFile$(data.filePath, data.sortedSourceFile))
+        );
+        const allFinishedWrites$ = forkJoinObservable(allWrites$).pipe(mapObservable(_ => { return; }));
+        return allFinishedWrites$.toPromise();
     }
 
-    private getSortedSource(filePath: string, fileSource: string) {
+    private getSortedSource(filePath: string, fileSource: string): string {
         const imports = this.walker.parseImports(filePath, fileSource);
         if (!imports.length) {
-            return;
+            return null;
         }
         const sortedImports = this.sorter.sortImportElements(imports);
         const importText = this.importCreator.createImportText(sortedImports.groups);
-        const numberOfImportLines = importText.split('\n').length;
-
-        return io.writeFile(filePath, importText);
+        return importText;
     }
 
-    private getAllFilePathsUnderThePath(srcPath: string): Promise<string[]> {
-        if (!srcPath) {
+    private allFilesUnderThePath$(startingSourcePath: string): Observable<{ filePath: string, file: string }> {
+        const allFilePaths$ = this.allFilePathsUnderThePath$(startingSourcePath);
+        const fileData$ = allFilePaths$.pipe(
+            switchMapObservable(filePaths => {
+                const files$ = filePaths.map(path => io.readFile$(path).pipe(mapObservable(file => ({ filePath: path, file: file }))));
+                return files$;
+            }),
+            mergeAll()
+        );
+        return fileData$;
+    }
+
+    private allFilePathsUnderThePath$(startingSourcePath: string): Observable<string[]> {
+        if (!startingSourcePath) {
             throw new Error('No directory selected.');
         }
 
         const allFilesPatterns = ['**/*.ts', '**/*.tsx'];
-        const excludes = [];
-        const filesAsync = allFilesPatterns.map(pattern => io.getFiles(srcPath, pattern, excludes));
-        return Promise.all(filesAsync).then(files => flatMap(files));
+        const ignore = [];
+        const filesPaths$ = allFilesPatterns.map(pattern => io.filePaths$(startingSourcePath, pattern, ignore));
+        return mergeObservable(...filesPaths$);
     }
 }
--- src/helpers/io.ts
+++ src/helpers/io.ts
@@ -1,38 +1,81 @@
 import * as fs from 'fs';
 import * as glob from 'glob';
 import * as path from 'path';
+import { Observable, Observer } from 'rxjs';
 
-export function getFullPath(srcPath: string, filename: string) {
+export function readFile$(path: string, encoding: string = 'utf-8'): Observable<string> {
+    return Observable.create((observer: Observer<string>) => {
+        fs.readFile(path, encoding, (error, data) => {
+            if (error) {
+                observer.error(error);
+            } else {
+                observer.next(data);
+                observer.complete();
+            }
+        });
+    });
+}
+
+export function writeFile$(path: string, data: string): Observable<void> {
+    return Observable.create((observer: Observer<void>) => {
+        fs.writeFile(path, data, (error) => {
+            if (error) {
+                observer.error(error);
+            } else {
+                observer.complete();
+            }
+        });
+    });
+}
+
+export function getFullPath(srcPath: string, filename: string): string {
     return path.join(srcPath, filename);
 }
 
-export function getFiles(srcPath: string, pattern: string, ignore: string | string[]): Promise<string[]> {
-    const results = new Promise<string[]>((resolve, reject) => {
+export function filePaths$(startingSourcePath: string, pattern: string, ignore: string | string[]): Observable<string[]> {
+    return Observable.create((observer: Observer<string[]>) => {
         glob(
             pattern,
             {
-                cwd: srcPath,
+                cwd: startingSourcePath,
                 ignore,
                 nodir: true
             },
-            (error, matches) => error ? reject(error) : resolve(matches)
-        );
-    });
-    return results.then(filePaths => filePaths.map(filePath => getFullPath(srcPath, filePath)));
-}
-
-export function readFile(filePath: string, encoding: string = 'utf-8'): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-        fs.readFile(filePath, encoding, (error, data) => error ? reject(error) : resolve(data));
-    });
-}
-
-export function exists(filePath: string) {
-    return fs.existsSync(filePath);
-}
-
-export function writeFile(filePath: string, data: string): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        fs.writeFile(filePath, data, (error) => error ? reject(error) : resolve());
+            (error, matches) => {
+            if (error) {
+                observer.error(error);
+            } else {
+                const fullPaths = matches.map(filePath => getFullPath(startingSourcePath, filePath));
+                observer.next(fullPaths);
+                observer.complete();
+            }
+        });
     });
 }
+
+// export function getFiles(srcPath: string, pattern: string, ignore: string | string[]): Promise<string[]> {
+//     const results = new Promise<string[]>((resolve, reject) => {
+//         glob(
+//             pattern,
+//             {
+//                 cwd: srcPath,
+//                 ignore,
+//                 nodir: true
+//             },
+//             (error, matches) => error ? reject(error) : resolve(matches)
+//         );
+//     });
+//     return results.then(filePaths => filePaths.map(filePath => getFullPath(srcPath, filePath)));
+// }
+
+// export function readFile(filePath: string, encoding: string = 'utf-8'): Promise<string> {
+//     return new Promise<string>((resolve, reject) => {
+//         fs.readFile(filePath, encoding, (error, data) => error ? reject(error) : resolve(data));
+//     });
+// }
+
+// export function writeFile(filePath: string, data: string): Promise<void> {
+//     return new Promise<void>((resolve, reject) => {
+//         fs.writeFile(filePath, data, (error) => error ? reject(error) : resolve());
+//     });
+// }
